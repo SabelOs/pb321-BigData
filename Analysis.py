14c72cb2729e56f66612613f060180c5006da5ac
Soeke Sabelus <soekesabelus@gmail.com>
2026-01-09 10:36:47 +0100

    Created first test plot and world map with population as false colors

---
+++ Analysis.py
@@ -0,0 +1,191 @@
+#%%
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import geopandas as gpd
+import geodatasets
+from rapidfuzz import process, fuzz
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib as mpl
+
+def find(df, column, text):
+    return df.loc[df[column].str.contains(text, case=False, na=False)]
+# %%
+file = "WPP2024_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT.xlsx"
+
+df = pd.read_excel(file)
+print(df.dtypes)
+# %%
+
+df = df.set_axis(df.iloc[15],axis=1)
+
+print(df.dtypes)
+#%%
+df.drop(axis = 0, index = df.index[:16],inplace = True)
+
+df.reset_index(drop=True)
+# %%
+df['Type'].unique()
+# %%
+df_countries = df[df["Type"] == "Country/Area"]
+# %%
+country_col = "Region, subregion, country or area *"
+pop_col = "Total Population, as of 1 July (thousands)"
+
+pop_table = df_countries.pivot(index="Year", columns=country_col, values=pop_col)
+# %%
+plt.figure(figsize=(12, 6))
+
+for country in pop_table.columns:
+    plt.plot(pop_table.index, pop_table[country], alpha=0.3,label=str(country))
+
+plt.xlabel("Year")
+plt.ylabel("Population (thousands)")
+plt.title("Population of All Countries Over Time")
+plt.tight_layout()
+plt.legend()
+plt.show()
+# %%
+plt.figure(figsize=(12, 6))
+
+for country in ['China', 'Germany', 'Ethiopia']:
+    plt.plot(pop_table.index, pop_table[country]/max(pop_table[country]), alpha=0.3,label=str(country))
+
+plt.xlabel("Year")
+plt.ylabel("Population (thousands)")
+plt.title("Population of All Countries Over Time")
+plt.tight_layout()
+plt.legend()
+plt.show()
+# %%
+url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+world = gpd.read_file(url).rename({'ADMIN': 'name'}, axis = "columns")
+world.plot(column='name',edgecolor='black')
+print(world.columns)
+# %%
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+
+water = 'lightskyblue'
+earth = 'tan'
+
+ax.set_facecolor(water)
+ax.set_xlim([-20,40])
+ax.set_ylim([25,80])
+
+world[world['name']!='France'].plot(ax=ax, edgecolor='grey', facecolor=earth, linewidth=1, alpha = 1) 
+# %%
+# %% Create Plot for 2023 Population
+year = 2023
+
+pop_year = df_countries[df_countries["Year"] == year][
+    [
+        "Region, subregion, country or area *",
+        "Total Population, as of 1 July (thousands)"
+    ]
+].copy()
+
+pop_year.columns = ["country", "population"]
+
+#Manually reasing some of the names which are diffrent between the map and UN data
+rename_map = {
+    #Scheme: pop_year : world_pop
+    "Viet Nam": "Vietnam",
+    "Lao People's Democratic Republic": "Laos",
+    "Bolivia (Plurinational State of)": "Bolivia",
+    "Republic of Moldova": "Moldova",
+    "Republic of Korea": "South Korea",
+    "Congo": "Republic of the Congo",   # adjust if needed
+    "Türkiye": "Turkey",
+    "Eswatini": "Eswatini",  # needed if map uses Swaziland
+    "Brunei Darussalam" : "Brunei",
+    "Timor-Leste" : "East Timor",
+    "Iran (Islamic Republic of)" : "Iran",
+    "Russian Federation": "Russia",
+    "Kosovo (under UNSC res. 1244)" : "Kosovo",
+    "Dem. People's Republic of Korea" : "North Korea",
+    "State of Palestine" : "Palestine",
+    "Syrian Arab Republic" : "Syria",
+    "China, Taiwan Province of China" : "Taiwan",
+    "Bahamas" : "The Bahamas",
+    "United States" : "United States of America",
+    "Venezuela (Bolivarian Republic of)" : "Venezuela",
+    "Eswatini" : "eSwatini",
+    "Serbia" : "Republic of Serbia",
+    "Falkland Islands (Malvinas)" : "Falkland Islands",
+    "Côte d'Ivoire" : "Ivory Coast"
+}
+
+pop_year["country"] = pop_year["country"].replace(rename_map)
+
+world_pop = world.merge(
+    pop_year,
+    how="left",
+    left_on="name",
+    right_on="country"
+)
+
+# Here we need to copy data as the UN does not distinguish between them: Northern Cyprus → Cyprus
+cyprus_pop = world_pop.loc[
+    world_pop["name"] == "Cyprus", "population"
+].values
+
+if len(cyprus_pop) == 1:
+    world_pop.loc[
+        world_pop["name"] == "Northern Cyprus", "population"
+    ] = cyprus_pop[0]
+
+# Somaliland → Somalia
+somalia_pop = world_pop.loc[
+    world_pop["name"] == "Somalia", "population"
+].values
+
+if len(somalia_pop) == 1:
+    world_pop.loc[
+        world_pop["name"] == "Somaliland", "population"
+    ] = somalia_pop[0]
+
+uncolored = world_pop[world_pop["population"].isna()]
+
+print("\nRegions in map WITHOUT population data:\n")
+for name in sorted(uncolored["name"].unique()):
+    print(name)
+
+print(f"\nTotal uncolored regions: {uncolored['name'].nunique()}")
+
+
+fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1)
+
+# Plot WITHOUT legend
+world_pop.plot(
+    column="population",
+    cmap="Blues",
+    linewidth=0.4,
+    ax=ax,
+    edgecolor="black",
+    missing_kwds={
+        "color": "lightgrey",
+        "label": "No data"
+    },
+    legend=False
+)
+
+# Create proper colorbar
+norm = mpl.colors.Normalize(
+    vmin=world_pop["population"].min(),
+    vmax=world_pop["population"].max()
+)
+
+sm = mpl.cm.ScalarMappable(norm=norm, cmap="Blues")
+sm._A = []  # required for older matplotlib
+
+cbar = fig.colorbar(sm, cax=cax)
+cbar.set_label("Population (thousands)")
+
+ax.set_title(f"World Population by Country ({year})", fontsize=14)
+ax.axis("off")
+
+plt.show()
+# %%
